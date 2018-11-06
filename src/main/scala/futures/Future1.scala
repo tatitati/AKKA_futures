27a3fbdb25b94cf7850b93c013e6aab357d793cd
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-06 18:02:25 +0000

    tydi

--- src/main/scala/futures/Future1.scala
+++ src/main/scala/futures/Future1.scala
@@ -1,16 +1,12 @@
 package Futures
 
-
 import scala.concurrent.{Await, Future}
-import scala.util.Random
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class Future1 {
 
 	def simulateBlockingHttpRequest(): String = {
-		implicit val baseTime = System.currentTimeMillis
-
 		val responseFuture = Future {
 			Thread.sleep(500) // 0.5 secs
 			"BLOCKED FUTURE DONE"
@@ -20,8 +16,6 @@ class Future1 {
 	}
 
 	def simulateNonBlockingHttpRequest(): Future[String] = {
-		implicit val baseTime = System.currentTimeMillis
-
 		Future {
 			Thread.sleep(500) // 0.5 secs
 			"NON BLOCKED FUTURE DONE"

ecd2e226b8064746743da143bcf30c1cc5f3055f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-05-15 22:36:40 +0100

    simplified, merged actor definition in tests, is just a learning project shit

--- src/test/scala/actors/tellAskPattern/ActorASpec.scala
+++ src/test/scala/actors/tellAskPattern/ActorASpec.scala
@@ -8,6 +8,25 @@ import akka.util.Timeout
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
+import akka.actor.Actor
+
+class ActorA extends Actor {
+  override def receive: Receive = {
+    case "identifyWithTell" => {
+      sender ! "I'm ActorA"
+    }
+
+    case "identifyWithAsk" => {
+      sender ! "I'm ActorA"
+    }
+
+    case "identifyWithAskAndBigDelay" => {
+      Thread.sleep(5000) // 5 secs
+      println("ActorA: delay of 5secs complete")
+      sender ! "I'm ActorA"
+    }
+  }
+}
 
 class ActorASpec extends FunSuite with Matchers {
   val actorSystem = ActorSystem("ChildActorTest")

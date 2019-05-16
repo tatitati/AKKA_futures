6cb952df77bb2c9bddd0a1cef8fe6aabeec1ea0d
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-05-16 19:20:03 +0100

    more

---
+++ src/test/scala/actors/LoggingInActorSpec.scala
@@ -0,0 +1,22 @@
+package actors
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import org.scalatest.FunSuite
+
+class ActorBA extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case "ping" => {
+      log.info("loggin something")
+      log.warning("loggin something")
+    }
+  }
+}
+
+class LoggingInActorSpec extends FunSuite {
+  test("I can create an actor system") {
+    val actorSystem = ActorSystem("MyActorSystem")
+    var actorBA = actorSystem.actorOf(Props[ActorBA])
+
+    actorBA ! "ping"
+  }
+}

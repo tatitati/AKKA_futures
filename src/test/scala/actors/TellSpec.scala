2b802749d2ca06e4a16df3aff02b4f5ef48a67f6
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-05-16 22:40:01 +0100

    more

--- src/test/scala/actors/TellSpec.scala
+++ src/test/scala/actors/TellSpec.scala
@@ -1,12 +1,16 @@
 package actors
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorSystem, DeadLetter, Props}
 import org.scalatest.FunSuite
 
 class ActorCA extends Actor with ActorLogging {
   override def receive: Receive = {
     case "ping" => {
       sender ! "whatever"
+    }
+    case DeadLetter(msg, from, to) => {
+      // because when called with ! this is not gooing to return to sender, we can see here our "whatever" msg
+      println("DEADLETTER: " + msg)
     }
   }
 }
@@ -20,5 +24,15 @@ class TellSpec extends FunSuite {
     val response = actorCA ! "ping"
 
     assert(response.isInstanceOf[Unit])
+  }
+
+  test("If used !, then deadletters is used in response") {
+    val actorSystem = ActorSystem("MyActorSystem")
+
+    var actorCA = actorSystem.actorOf(Props[ActorCA])
+    actorSystem.eventStream.subscribe(actorCA, classOf[DeadLetter])
+
+    val response = actorCA ! "ping"
+
   }
 }

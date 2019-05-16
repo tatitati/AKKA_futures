001ddafd09ec4f21efcffa204708944141a73a6f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-05-16 19:34:11 +0100

    more

--- src/test/scala/actors/TellSpec.scala
+++ src/test/scala/actors/TellSpec.scala
@@ -1,7 +1,23 @@
 package actors
 
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import org.scalatest.FunSuite
 
-class TellSpec extends FunSuite {
+class ActorCA extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case "ping" => {
+      sender ! "whatever"
+    }
+  }
+}
 
+class TellSpec extends FunSuite {
+  test("Tell/Bang/! never receives a response") {
+    val actorSystem = ActorSystem("MyActorSystem")
+    var actorCA = actorSystem.actorOf(Props[ActorCA])
+
+    val response = actorCA ! "ping"
+
+    assert(response.isInstanceOf[Unit])
+  }
 }

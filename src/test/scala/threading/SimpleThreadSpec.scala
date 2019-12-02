7cedae8f0df079b5ec16a0278a8bda2b49e74145
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-12-02 22:33:56 +0000

    more

--- src/test/scala/threading/SimpleThreadSpec.scala
+++ src/test/scala/threading/SimpleThreadSpec.scala
@@ -3,27 +3,24 @@ package threading
 import org.scalatest.FunSuite
 
 class SimpleThreadSpec extends FunSuite {
-  test("can create a very basic thread"){
-    class MyThread extends Thread {
+
+  test("I can create a very basic thread"){
+    val t = new Thread {
       override def run(): Unit = {
-        println("this is my FIRST thread")
+        println("this is my FIRST thread :: "+ "Thread id: " + Thread.currentThread.getId)
       }
     }
 
-    val t = new MyThread()
     t.start()
     t.join()
     println("done")
   }
 
-  test("Another way"){
+  test("In this way I don't need to start() the thread, it starts automatically"){
     val t = new Thread {
-      override def run(): Unit = {
-        println("this is my SECOND thread")
-      }
+      println("this is my SECOND thread :: "+ "Thread id: " + Thread.currentThread.getId)
     }
 
-    t.start()
     t.join()
     println("done")
   }

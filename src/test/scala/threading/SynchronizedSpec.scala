2506378bc26fa4675b2613d44c15a3d467e2d5ce
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-12-04 21:03:58 +0000

    more

--- src/test/scala/threading/SynchronizedSpec.scala
+++ src/test/scala/threading/SynchronizedSpec.scala
@@ -3,83 +3,83 @@ package threading
 import org.scalatest.FunSuite
 
 class SynchronizedSpec extends FunSuite {
-  test("Dead race scenario") {
-    var num = 0
-    def workerIncrement(): Int = {
-      for (_ <- 1 to 100000) {
-        num = num + 1
-      }
-      num
-    }
+  //test("Dead race scenario") {
+  //  var num = 0
+  //  def workerIncrement(): Int = {
+  //    for (_ <- 1 to 100000) {
+  //      num = num + 1
+  //    }
+  //    num
+  //  }
+  //
+  //  val t1 = new Thread{
+  //    override def run(): Unit = workerIncrement()
+  //  }
+  //  val t2 = new Thread{
+  //    override def run(): Unit = workerIncrement()
+  //  }
+  //  val t3 = new Thread{
+  //    override def run(): Unit = workerIncrement()
+  //  }
+  //  val t4 = new Thread{
+  //    override def run(): Unit = workerIncrement()
+  //  }
+  //
+  //  val threadsPool = List(t1, t2, t3, t4)
+  //  threadsPool.map(_.start())
+  //  threadsPool.map(_.join())
+  //
+  //  println(num)
+  //
+  //  // OUTPUT:
+  //  // 115789
+  //  // 15885
+  //  // 13383
+  //}
+  //
+  //test("Synchronization with Lock scenario") {
+  //  var num = 0
+  //  def workerIncrement(): Int = {
+  //    this.synchronized{ // we changed only this line
+  //      for (_ <- 1 to 100000) {
+  //        num = num + 1
+  //      }
+  //      num
+  //    }
+  //  }
+  //
+  //  val t1 = new Thread{
+  //    override def run(): Unit = workerIncrement()
+  //  }
+  //  val t2 = new Thread{
+  //    override def run(): Unit = workerIncrement()
+  //  }
+  //  val t3 = new Thread{
+  //    override def run(): Unit = workerIncrement()
+  //  }
+  //  val t4 = new Thread{
+  //    override def run(): Unit = workerIncrement()
+  //  }
+  //
+  //  val threadsPool = List(t1, t2, t3, t4)
+  //  threadsPool.map(_.start())
+  //  threadsPool.map(_.join())
+  //
+  //  println(num)
+  //
+  //  // OUTPUT:
+  //  // 500000
+  //  // 500000
+  //  // 500000
+  //}
 
-    val t1 = new Thread{
-      override def run(): Unit = workerIncrement()
-    }
-    val t2 = new Thread{
-      override def run(): Unit = workerIncrement()
-    }
-    val t3 = new Thread{
-      override def run(): Unit = workerIncrement()
-    }
-    val t4 = new Thread{
-      override def run(): Unit = workerIncrement()
-    }
-
-    val threadsPool = List(t1, t2, t3, t4)
-    threadsPool.map(_.start())
-    threadsPool.map(_.join())
-
-    println(num)
-
-    // OUTPUT:
-    // 115789
-    // 15885
-    // 13383
-  }
-
-  test("Synchronization with Lock scenario") {
-    var num = 0
-    def workerIncrement(): Int = {
-      this.synchronized{ // we changed only this line
-        for (_ <- 1 to 100000) {
-          num = num + 1
-        }
-        num
-      }
-    }
-
-    val t1 = new Thread{
-      override def run(): Unit = workerIncrement()
-    }
-    val t2 = new Thread{
-      override def run(): Unit = workerIncrement()
-    }
-    val t3 = new Thread{
-      override def run(): Unit = workerIncrement()
-    }
-    val t4 = new Thread{
-      override def run(): Unit = workerIncrement()
-    }
-
-    val threadsPool = List(t1, t2, t3, t4)
-    threadsPool.map(_.start())
-    threadsPool.map(_.join())
-
-    println(num)
-
-    // OUTPUT:
-    // 500000
-    // 500000
-    // 500000
-  }
-
-  test("A Thread.sleep inside of a synchronized doesnt produce a change to another thread") {
+  test("A Thread.sleep inside of a synchronized doesnt produce a change to another thread becauase it owns the lock still") {
     var num = 0
     def increment(): Int = {
       this.synchronized{ // we changed only this line
-        println("this Thread id before waiting: " + Thread.currentThread.getId)
-        Thread.sleep(407700)
-        println("this Thread id after waiting: " + Thread.currentThread.getId)
+        println("Thread Id: " + Thread.currentThread.getId +" started")
+        Thread.sleep(7700)
+        println("Thread Id: " + Thread.currentThread.getId +" finished")
         for (_ <- 1 to 100000) {
           num = num + 1
         }

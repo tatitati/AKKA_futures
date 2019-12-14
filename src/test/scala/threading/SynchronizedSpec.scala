399194179ade7e2c6a5ec19eed31878a5a77e846
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-12-14 17:42:37 +0000

    more

--- src/test/scala/threading/SynchronizedSpec.scala
+++ src/test/scala/threading/SynchronizedSpec.scala
@@ -2,8 +2,32 @@ package threading
 
 import org.scalatest.FunSuite
 
-class Synchronized2Spec extends FunSuite {
-  test("A Thread.sleep inside of a synchronized doesnt produce a change to another thread becauase it owns the lock still") {
+class SynchronizedSpec extends FunSuite {
+
+  test("writes to *num in a synchronized way to avoid race scenario") {
+    var num = 0
+    def workerIncrement(): Unit = this.synchronized {
+      for (_ <- 1 to 100000) {
+        num = num + 1
+      }
+    }
+
+    val threadsPool = List.tabulate(4){ _ =>
+      new Thread{
+        override def run(): Unit = workerIncrement()
+      }
+    }
+
+    threadsPool.map(_.start())
+    threadsPool.map(_.join())
+
+    println(num)
+
+    // OUTPUT:
+    // 400000
+  }
+
+  test("A Thread.sleep() inside of a synchronized doesnt produce a change to another thread becauase it owns the lock during the sleep period") {
 
     def workerIncrement(): Unit = this.synchronized {
       println("Thread Id: " + Thread.currentThread.getId +" started")

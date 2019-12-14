dd036186cac9ae92d5b879115076ba0d2157ee90
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-12-14 16:20:49 +0000

    more

---
+++ src/test/scala/threading/ProducerConsumerBussyWaitingSpec.scala
@@ -0,0 +1,46 @@
+package threading
+
+import org.scalatest.FunSuite
+
+class ProducerConsumerBussyWaitingSpec extends FunSuite {
+
+  class Container {
+    private var value: Int = 0
+
+    def isEmpty: Boolean = value == 0
+    def set(newValue: Int) = value = newValue
+    def get = {
+      val result = value
+      value = 0
+      result
+    }
+  }
+
+  def producerConsumer(): Unit = {
+    val container = new Container
+    val consumer = new Thread(() => {
+      println("\t[consumer] waiting...")
+      while(container.isEmpty){
+        println("\t[consumer] actively waiting...")
+      }
+
+      println("\t[consumer] I have consummed" + container.get)
+    })
+
+    val producer = new Thread(() => {
+      println("[producer] computing...")
+      Thread.sleep(500)
+      container.set(42)
+      println("[producer] I have produced after long work the value: 42")
+    })
+
+    consumer.start()
+    producer.start()
+    consumer.join()
+    producer.join()
+  }
+
+  test("run"){
+    producerConsumer()
+  }
+}

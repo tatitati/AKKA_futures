86a63137cf37e9282b5eb5a709577d613e96261f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-12-16 00:20:56 +0000

    more

--- src/test/scala/threading/NotifyAllSpec.scala
+++ src/test/scala/threading/NotifyAllSpec.scala
@@ -4,32 +4,32 @@ import org.scalatest.FunSuite
 
 class NotifyAllSpec extends FunSuite {
 
-  def runBells() = {
-    val bell = new Object()
+  test("with .notifyAll()"){
+    def runBells() = {
+      val bell = new Object()
 
-    val waiters = List.tabulate(5){ i =>
-      new Thread(() => {
-        bell.synchronized {
-          println(s"\t[Thread $i] waiting...")
-          bell.wait()
-          println(s"\t[Thread $i] resumed...")
+      val waiters = List.tabulate(5){ i =>
+        new Thread(() => {
+          bell.synchronized {
+            println(s"\t[Thread $i] waiting...")
+            bell.wait()
+            println(s"\t[Thread $i] resumed...")
+          }
+        })
+      }
+
+      val activator = new Thread(() => {
+        println("[Main] activating ....")
+        Thread.sleep(2000)
+        bell.synchronized{
+          bell.notifyAll()
         }
       })
+
+      waiters.map(_.start()); activator.start()
+      waiters.map(_.join()); activator.join()
     }
 
-    val activator = new Thread(() => {
-      println("[Main] activating ....")
-      Thread.sleep(2000)
-      bell.synchronized{
-        bell.notifyAll()
-      }
-    })
-
-    waiters.map(_.start()); activator.start()
-    waiters.map(_.join()); activator.join()
-  }
-
-  test("run"){
     runBells()
 
     // Output with NotifyAll()
@@ -45,6 +45,42 @@ class NotifyAllSpec extends FunSuite {
     //  [Thread 2] resumed...
     //  [Thread 1] resumed...
     //  [Thread 0] resumed...
+  }
 
+  test("with .notify()") {
+    def runBells() = {
+      val bell = new Object()
+
+      val waiters = List.tabulate(5){ i =>
+        new Thread(() => {
+          bell.synchronized {
+            println(s"\t[Thread $i] waiting...")
+            bell.wait()
+            println(s"\t[Thread $i] resumed...")
+          }
+        })
+      }
+
+      val activator = new Thread(() => {
+        println("[Main] activating ....")
+        Thread.sleep(2000)
+        bell.synchronized{
+          bell.notify()
+        }
+      })
+
+      waiters.map(_.start()); activator.start()
+      waiters.map(_.join()); activator.join()
+    }
+
+    runBells()
+
+    //    [Thread 0] waiting...
+    //    [Thread 2] waiting...
+    //    [Thread 1] waiting...
+    //    [Thread 3] waiting...
+    //    [Thread 4] waiting...
+    //  [Main] activating ....
+    //    [Thread 0] resumed...
   }
 }

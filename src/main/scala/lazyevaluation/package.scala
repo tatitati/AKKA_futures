b43e0ab81d156eb63627b79e80d0d94534187571
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-01-04 00:06:54 +0000

    more

--- src/main/scala/lazyevaluation/package.scala
+++ src/main/scala/lazyevaluation/package.scala
@@ -15,9 +15,9 @@ object Exercises extends App {
     def tail: MyStream[A]
 
     def #::[B >: A](element: B): MyStream[B] // prepent operator
-    def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // contaenate two streams
+    def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // contatenate two streams
 
-    def foreachf(f: A => Unit): Unit
+    def foreach(f: A => Unit): Unit
     def map[B](f: A => B): MyStream[B]
     def flatMap[B](f: A => MyStream[B]): MyStream[B]
     def filter(predicate: A => Boolean): MyStream[A]
@@ -28,5 +28,39 @@ object Exercises extends App {
 
   object MyStream {
     def from[A](start: A)(generator: A => A): MyStream[A] = ???
+  }
+
+  object EmptyMyStream extends MyStream[Nothing] {
+    def isEmpty: Boolean = true
+    def head: Nothing = throw new NoSuchElementException
+    def tail: MyStream[Nothing] = throw new NoSuchElementException
+
+    def #::[B >: Nothing](element: B): MyStream[B] = new Cons(element, this)
+    def ++[B >: Nothing](anotherStream: MyStream[B]): MyStream[B] = anotherStream
+
+    def foreach(f: Nothing => Unit): Unit = ()
+    def map[B](f: Nothing => B): MyStream[B] = this
+    def flatMap[B](f: Nothing => MyStream[B]): MyStream[B] = this
+    def filter(predicate: Nothing => Boolean): MyStream[Nothing] = this
+
+    def take(n: Int):MyStream[Nothing] = this
+    def takeAsList(n: Int):List[Nothing] = Nil
+  }
+
+  class Cons[+A](hd: A, tl: => MyStream[A]) extends MyStream[A] {
+    def isEmpty: Boolean  = false
+    def head: A = ???
+    def tail: MyStream[A] = ???
+
+    def #::[B >: A](element: B): MyStream[B] = ???
+    def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] = ???
+
+    def foreach(f: A => Unit): Unit = ???
+    def map[B](f: A => B): MyStream[B] = ???
+    def flatMap[B](f: A => MyStream[B]): MyStream[B] = ???
+    def filter(predicate: A => Boolean): MyStream[A] = ???
+
+    def take(n: Int):MyStream[A] = ???
+    def takeAsList(n: Int):List[A] = ???
   }
 }
